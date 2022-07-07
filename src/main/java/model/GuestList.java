d33eb44f413ab3d9f88844b616fe3635b36d0fa2
asrarbutt <asrarbutt1986@gmail.com>
2022-07-08 00:30:01 +0200

    done test1

---
+++ src/main/java/model/GuestList.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.List;
+
+public class GuestList {
+    private List<String> guest;
+
+    public void setGuests(List<String> guest) {
+        this.guest=guest;
+
+    }
+
+
+    public List<String> getGuests() {
+        return guest;
+    }
+
+
+}
---
+++ src/test/java/model/FilebasedTest.java
@@ -0,0 +1,39 @@
+package model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class FilebasedTest {
+
+
+    @Test
+    void shouldBeEmptyInitially(){
+
+        //Given
+        GuestList party= new GuestList();
+        party.setGuests(Collections.emptyList());
+
+        //when
+        List<String> actual=party.getGuests();
+
+       //then
+        Assertions.assertEquals(Collections.emptyList(), actual);
+
+    }
+
+    @Test
+    void shouldReadSameGuestsAsWrittenBefore(){
+
+        //Given
+        GuestList party= new GuestList();
+        party.setGuests(List.of("Karl", "Ute"));
+
+
+
+
+    }
+}
